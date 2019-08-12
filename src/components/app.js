46c8149b0e9eb2988d2b2f9b382fd05954aa1e65
Josh Wilkerson <jgwilkerson@gmail.com>
2019-08-12 15:19:43 -0500

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import IntegrationChecker from './integration_checker'
+
+const App = () => {
+	return (
+		<div style={{ padding: 20 }}>
+			<IntegrationChecker />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/display_embed_form.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+export default function DisplayEmbedForm(props) {
+	const { data, app } = props
+	const script = `https://js.churchcenter.com/modal/v1`
+	const url = data.replace(/\s/g, '')
+
+	return (
+		<div>
+			<Helmet
+				script={[
+					{
+						type: 'text/javascript',
+						src: script,
+					},
+				]}
+			/>
+			<h2>{app} form</h2>
+			<a href={url} data-open-in-church-center-modal="true">
+				Open modal
+			</a>
+			<a
+				href={url}
+				rel="noopener noreferrer"
+				target="_blank"
+				style={{ marginLeft: 10 }}
+			>
+				Open new tab
+			</a>
+		</div>
+	)
+}
